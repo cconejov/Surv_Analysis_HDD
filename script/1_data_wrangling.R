b78935e476b3eba8f33c338b88eecb4636e9a0bd
cconejov <cconejov@gmail.com>
2020-03-29 20:03:32 -0600

    Update Data Wrangling. Add Smart_9_Raw, and change the varaible age and study_time

--- script/1_data_wrangling.R
+++ script/1_data_wrangling.R
@@ -13,7 +13,7 @@ file_names <- list.files("data/drive_stats_2019",
 data <- rbindlist(lapply(file_names,function(x) fread(input = x,
                                                     header = TRUE,
                                                     stringsAsFactors = FALSE,
-                                                    select = c("date", "serial_number", "model","capacity_bytes","failure") ))
+                                                    select = c("date", "serial_number", "model","capacity_bytes","failure", "smart_9_raw") ))
                   )
 
 data <- data %>%
@@ -21,42 +21,53 @@ data <- data %>%
          capacity_bytes = round(capacity_bytes/10e11)
          )
 
+## Group of data
+
+max_hour_smart_9_raw <- max(data$smart_9_raw[!is.na(data$smart_9_raw)]) + 1
+
+
 data_group <- data %>% 
-  group_by(serial_number, model, capacity_bytes) %>% 
-  summarise(count_obs = n(),
+  group_by(serial_number, model) %>% 
+  summarise(TB = max(capacity_bytes),
+            count_obs = n(),
             min_date = min(date),
             max_date = max(date),
+            min_Hours = min(smart_9_raw),
+            max_Hours = max(smart_9_raw),
             count_fail = sum(failure),
             fail       = max(failure),
-            first_date_fail = min(ifelse(failure == 1,date,"2020-01-01")))
+            first_date_fail = min(ifelse(failure == 1, date,"2020-01-01")),
+            first_hour_fail = min(ifelse(failure == 1, smart_9_raw, max_hour_smart_9_raw))
+  )
 
 
-begin_study <- "2019-01-01"
+
+## Independent variables
 
 data_group <- data_group %>% 
-                mutate(age = difftime(min_date, begin_study, units = c("days")),
-                       study_time = ifelse(fail == 1,
-                                           difftime(first_date_fail, min_date, units = c("days")), 
-                                           difftime(max_date, min_date, units = c("days")))   + 1
-                       )
+  mutate(age = floor(min_Hours/24),
+         study_time = ifelse(fail == 1,
+                             floor((first_hour_fail - min_Hours)/24), 
+                             floor((max_Hours - min_Hours)/24)) + 1 
+  )
 
 write.csv(x =  data_group,
           file = "output/data/data_group_2019.csv")
 
 
-problem_raw <- data %>% filter(serial_number == "ZCH07Q3L")
+problem_raw <- data %>% filter(serial_number == "ZA153SZQ")
 
 #ZCH07Q3L / ZA153SZQ
 
 ## save register for hiphotesis taken
 
 write.csv(x =  problem_raw,
-          file = "output/data/problem_HDD.csv")
+          file = "output/data/problem_HDD_2Fails.csv")
 
 
 # Multiple register: 2AG8AVRY, 2AGKYLBY
 
-problem <- data_group %>% filter(serial_number == "ZA153SZQ")
+problem_raw <- data_group %>% filter(serial_number == "2AG8AVRY")
 
 
 fails <- data_group %>% filter(count_fail > 0)
